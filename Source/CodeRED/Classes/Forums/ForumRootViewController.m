e225ce5672c83be597ce166d17cd83c40b768683
Tyler Stromberg <tylers@mac.com>
2011-11-02 22:30:02 -0400

    Add static content for root forum list

--- Source/CodeRED/Classes/Forums/ForumRootViewController.m
+++ Source/CodeRED/Classes/Forums/ForumRootViewController.m
@@ -8,15 +8,21 @@
 
 #import "ForumRootViewController.h"
 
+#import "Forum.h"
+
 @implementation ForumRootViewController
 
 @synthesize footerView = _footerView;
+
+
+#pragma mark - Object Lifecycle
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Release any cached data, images, etc that aren't in use.
 }
+
 
 #pragma mark - View Lifecycle
 
@@ -34,30 +40,57 @@
     [super viewDidUnload];
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
-}
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+#pragma mark - UITableView DataSource Methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return [Forum rootForum].childForums.count;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    Forum *currentForum = [[Forum rootForum].childForums objectAtIndex:section];
+    
+    UIView *backgroundView = [[UIView alloc] initWithFrame:CGRectZero];
+    backgroundView.backgroundColor = [UIColor blackColor];
+    backgroundView.frame = CGRectMake(0.0f, 0.0f, 320.0f, 22.0f);
+    
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    titleLabel.font = [UIFont boldSystemFontOfSize:16.0f];
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.backgroundColor = [UIColor clearColor];
+    titleLabel.opaque = NO;
+    titleLabel.text = currentForum.title;
+    titleLabel.frame = CGRectInset(backgroundView.bounds, 10.0f, 0.0f);
+    [backgroundView addSubview:titleLabel];
+    
+    return backgroundView;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    Forum *childForum = [[Forum rootForum].childForums objectAtIndex:section];
+    return childForum.childForums.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"ForumCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    Forum *parentForum = [[Forum rootForum].childForums objectAtIndex:indexPath.section];
+    Forum *childForum = [parentForum.childForums objectAtIndex:indexPath.row];
+    cell.textLabel.text = childForum.title;
+    
+    return cell;
 }
 
 @end
