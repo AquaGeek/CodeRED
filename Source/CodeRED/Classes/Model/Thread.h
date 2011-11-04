5a165e4ff32286a417f4f958ef418d7175315fe3
Tyler Stromberg <tylers@mac.com>
2011-11-03 21:02:43 -0400

    Add Thread model

---
+++ Source/CodeRED/Classes/Model/Thread.h
@@ -0,0 +1,13 @@
+//
+//  Thread.h
+//  CodeRED
+//
+//  Created by Tyler Stromberg on 11/3/11.
+//  Copyright (c) 2011 Lab 42 Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Thread : NSObject
+
+@end
---
+++ Source/CodeRED/Classes/Model/Thread.m
@@ -0,0 +1,13 @@
+//
+//  Thread.m
+//  CodeRED
+//
+//  Created by Tyler Stromberg on 11/3/11.
+//  Copyright (c) 2011 Lab 42 Development. All rights reserved.
+//
+
+#import "Thread.h"
+
+@implementation Thread
+
+@end
